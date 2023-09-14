20931f37cc1283898bb4fe7d51b08ae73234e5a2
jmontoya05 <santiagomontoya997@gmail.com>
2023-09-14 10:05:43 -0500

    Se crea modelo de base de datos, script de creación y diagrama relacional

---
+++ baseDeDatos/scripts/creacionBD.sql
@@ -0,0 +1,70 @@
+drop database if exists dbSitemaVuelos;
+create database dbSitemaVuelos;
+use dbSitemaVuelos;
+
+create table paises (
+	idPais int unsigned primary key auto_increment,
+    pais varchar(20) not null
+);
+
+create table ciudades (
+	idCiudad int unsigned primary key auto_increment,
+    ciudad varchar(30) not null,
+    idPais int unsigned not null,
+    foreign key (idPais) references paises(idPais)
+);
+
+create table aeropuertos (
+	idAeropuerto int unsigned primary key auto_increment,
+    nombreAeropuerto varchar(50) not null,
+    idCiudad int unsigned not null,
+    foreign key (idCiudad) references ciudades(idCiudad)
+);
+
+create table aerolineas (
+	idAerolinea int unsigned primary key auto_increment,
+    nombreAerolinea varchar(30) not null
+);
+
+create table tiposVuelo (
+	idTipoVuelo int unsigned primary key auto_increment,
+    tipoVuelo varchar(20) not null
+);
+
+create table vuelos (
+	idVuelo int unsigned primary key auto_increment,
+    codigoVuelo varchar(10) not null unique,
+    idAerolinea int unsigned not null,
+    idTipoVuelo int unsigned not null,
+    idAeropuertoOrigen int unsigned not null,
+    idAeropuertoDestino int unsigned not null,
+    fechaDeSalida datetime,
+    fechaDeLlegada datetime,
+    precio double,
+    asientosDisponibles int,
+    foreign key (idAerolinea) references aerolineas(idAerolinea),
+    foreign key (idTipoVuelo) references tiposVuelo(idTipoVuelo),
+    foreign key (idAeropuertoOrigen) references aeropuertos(idAeropuerto),
+    foreign key (idAeropuertoDestino) references aeropuertos(idAeropuerto)
+);
+
+create table pasajeros (
+	idPasajero int unsigned primary key auto_increment,
+    nombresPasajero varchar(50) not null,
+    apellidosPasajero varchar(50) not null,
+    pasaporte varchar(15) not null,
+    nacionalidad varchar(20) not null,
+    correo varchar(30),
+    telefono varchar(15) not null,
+    contactoEmergencia varchar(30) not null,
+    telefonoContacto varchar(15) not null
+);
+
+create table reservas (
+	idReserva int primary key auto_increment,
+    idVuelo int unsigned not null,
+    idPasajero int unsigned not null,
+    fechaReserva datetime,
+    foreign key (idVuelo) references vuelos(idVuelo),
+    foreign key (idPasajero) references pasajeros(idPasajero)
+);
