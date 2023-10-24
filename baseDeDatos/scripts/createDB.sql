e218cbdc59a662cc0c1c471718616cb2f1883d90
jmontoya05 <santiagomontoya997@gmail.com>
2023-10-23 19:16:16 -0500

    Se modifican diagramas de clases y entidad relación

---
+++ baseDeDatos/scripts/createDB.sql
@@ -0,0 +1,70 @@
+DROP DATABASE IF EXISTS flightReservationSystem;
+CREATE DATABASE flightReservationSystem;
+USE flightReservationSystem;
+
+CREATE TABLE countries (
+	country_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    country VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE cities (
+	city_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    ciudad VARCHAR(30) NOT NULL,
+    country_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(country_id)
+);
+
+CREATE TABLE airports (
+	airport_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    airport_name VARCHAR(50) NOT NULL,
+    city_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
+
+CREATE TABLE airlines (
+	airline_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    airline_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE flight_types (
+	flight_type_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    flight_type VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE flights (
+	flight_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    flight_code VARCHAR(10) NOT NULL UNIQUE,
+    airline_id INT UNSIGNED NOT NULL,
+    flight_type_id INT UNSIGNED NOT NULL,
+    airport_origin_id INT UNSIGNED NOT NULL,
+    airport_destination_id INT UNSIGNED NOT NULL,
+    departure_date DATETIME NOT NULL,
+    arrival_date DATETIME NOT NULL,
+    price DECIMAL(12,2) NOT NULL,
+    available_seats int NOT NULL,
+    FOREIGN KEY (airline_id) REFERENCES airlines(airline_id),
+    FOREIGN KEY (flight_type_id) REFERENCES flight_types(flight_type_id),
+    FOREIGN KEY (airport_origin_id) REFERENCES airports(airport_id),
+    FOREIGN KEY (airport_destination_id) REFERENCES airports(airport_id)
+);
+
+CREATE TABLE passengers (
+	passenger_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    passport VARCHAR(15) NOT NULL,
+    nationality VARCHAR(20) NOT NULL,
+    email VARCHAR(30),
+    phone_number VARCHAR(15) NOT NULL,
+    emergency_contact VARCHAR(30) NOT NULL,
+    contact_phone_number VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE reservations (
+	reservation_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    flight_id INT UNSIGNED NOT NULL,
+    passenger_id INT UNSIGNED NOT NULL,
+    reservation_date DATETIME NOT NULL,
+    FOREIGN KEY (flight_id) REFERENCES flights(flight_id),
+    FOREIGN KEY (passenger_id) REFERENCES passengers(passenger_id)
+);
