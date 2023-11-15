02b72ab419389b98840b8810bbb05f64eb873d2b
jmontoya05 <santiagomontoya997@gmail.com>
2023-11-14 22:03:31 -0500

    Se ajusta configuración del proyecto para solucionar error en el despliegue en azure

--- src/main/java/com/makaia/flightReservation/config/SecurityConfig.java
+++ src/main/java/com/makaia/flightReservation/config/SecurityConfig.java
@@ -36,11 +36,11 @@ public class SecurityConfig {
                                 // Restricciones específicas para rutas
                                 .antMatchers("/users", "/roles").hasAuthority("ADMIN")
                                 .antMatchers(HttpMethod.GET, "/flights/**").permitAll() // Todos pueden acceder
-                                .antMatchers(HttpMethod.GET, "/api/reservations/{reservationCode}", "/api/reservations/passenger/{passengerId}").hasAnyAuthority("USER")
-                                .antMatchers(HttpMethod.GET, "/api/reservations").hasAnyAuthority("ADMIN", "AIRLINE")
-                                .antMatchers(HttpMethod.POST, "/api/reservations/**").hasAnyAuthority("ADMIN", "AIRLINE", "USER")
+                                .antMatchers(HttpMethod.GET, "/reservations/{reservationCode}", "/reservations/passenger/{passengerId}").hasAnyAuthority("USER")
+                                .antMatchers(HttpMethod.GET, "/reservations").hasAnyAuthority("ADMIN", "AIRLINE")
+                                .antMatchers(HttpMethod.POST, "/reservations/**").hasAnyAuthority("ADMIN", "AIRLINE", "USER")
                                 // Todas las demás peticiones requieren autenticación
-                                .anyRequest().permitAll()
+                                .anyRequest().authenticated()
                 )
 
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
